c6423ad05aff6a24413b5b37310db85a19c2d3ec
Marc Benaiges <benaiges.marc@gmail.com>
2019-02-22 15:43:16 +0100

    first commit

--- main.js
+++ main.js
@@ -5,3 +5,44 @@ function main () {
 }
 
 window.addEventListener('load', main);
+$(window).scroll(function() {
+    // 100 = The point you would like to fade the nav in.
+      //FIRST IMAGE
+    if ($(window).scrollTop()  < 400 ){
+        
+        $('.bg-box').addClass('show');
+        
+    } else {
+        
+        $('.bg-box').removeClass('show');
+        
+        };
+    //SECOND IMAGE 
+    if ($(window).scrollTop()  > 400 && $(window).scrollTop() <800){
+       
+        $('.bg-paper').addClass('show');
+            
+    } else {
+            
+        $('.bg-paper').removeClass('show');
+            
+        };
+    //THIRD IMAGE
+    if ($(window).scrollTop()  > 800 ){
+       
+        $('.bg-parachute').addClass('show');
+                
+    } else {
+                
+        $('.bg-parachute').removeClass('show');
+                
+        };
+    });
+    
+    $('.scroll').on('click', function(e){		
+            e.preventDefault()
+        
+      $('html, body').animate({
+          scrollTop : $(this.hash).offset().top
+        }, 1500);
+    });
